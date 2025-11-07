ec8cea60940d5f8c7935f4148d435b3bc8475538
Prince <chourasiya8919@gmail.com>
2025-11-07 18:34:09 +0530

    requirments added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AIDocter",
+    version="0.1.0",
+    author="PRINCE",
+    author_email="chourasiya8919@gmail.com",
+    packages=find_packages(),
+    insatall_requires=[]
+)
